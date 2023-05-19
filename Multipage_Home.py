0992bb34ea8f10fbd3c1f564fbc6c4ae757ed516
vegegogi <bjeewon@naver.com>
2023-05-19 11:22:42 +0900

    third commit

--- Multipage_Home.py
+++ Multipage_Home.py
@@ -18,11 +18,11 @@ st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'>🐶서울특별시 반려동물 종합 플랫폼🐶</h1>", unsafe_allow_html=True)
 
 # 이미지 표시
-image_local = Image.open('.\data\main_banner.png')
+image_local = Image.open('main_banner.png')
 st.image(image_local, width=1050, use_column_width=True, caption='Image')
 
 # 배경 이미지 설정
-background_image = "data\main_banner.png"
+background_image = "main_banner.png"
 
 # CSS 스타일로 배경 이미지 적용
 css = f"""
